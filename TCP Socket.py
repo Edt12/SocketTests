fb47224bc6b22b6dac5a991a4e06b8ead70279e8
Edt12 <104518243+Edt12@users.noreply.github.com>
2023-08-30 20:52:57 +0100

    Add files via upload

--- TCP Socket.py
+++ TCP Socket.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 
 server=socket.socket(socket.AF_INET, socket.SOCK_STREAM)#Defines type of socket AF_INET =internet socket,SOCK_STREAM=TCP protocol#just for accepting connections not to be used to send
 Host=socket.getaddrinfo(socket.gethostname(),9090)
@@ -8,14 +9,40 @@ server.bind(('10.0.1.10',9090))
 server.listen(5)#if more than 5 connections waiting then reject
 #ONLY USE COMMUNICATION SOCKET TO TALK TO CLIENT SERVER IS ONLY FOR ACCEPTING CONNECTIONS
 
+Clients=[]
+Nicknames=[]
 
-while True:
-        print("stece")
-        (communication_socket, address)=server.accept()#returns address of client which connection and the clients comm socket
-        print(f"connected to {address}")
-        message=communication_socket.recv(1024).decode('utf-8')#1024 is buffur size
-        print(f"message from client is:{message}")
-        communication_socket.send(f"Got your message!Thank you!".encode('utf-8'))
-        communication_socket.close()
-        print(f"Connection with {address}ended!")
- 
+def SendToAll(message):#goes through all clients and sends them what server recieves so can see other peoples messages
+    for Client in Clients:
+        Client.send(message)
+        
+def ReceiveFromClient(Client):#run everytime a client connects to server
+   while True: 
+        try:#basically means doesnt stop when encounters an error
+            message=Client.recv(1024)
+            SendToAll(message)
+        except:#when it does encounter an error does this
+            index=Clients.index(Client)#gets index of client then removes it from client list 
+            Clients.remove(Client)
+            Nickname=Nicknames[index]#does same with nicknames and sends message to other clients saying person has left
+            SendToAll('{} left!'.format(Nickname).encode('ascii'))
+            Nicknames.remove(Nickname)
+            break
+def Listen():
+    while True:
+        #accepts connection with client 
+        client, address = server.accept()
+        print("Connected with {}".format(str(address)))
+        
+        client.send('GIB NICKNAME'.encode('ascii'))#sends message to client to transmit nickname to server
+        Nickname = client.recv(1024).decode('ascii')
+        Nicknames.append(Nickname)
+        Clients.append(Client)
+        
+        print("Nickname is {}",format(Nickname))
+        SendToAll("{} joined!",format(Nickname).encode('ascii'))
+        Client.send('Connected to Server!!!'.encode('ascii'))
+        
+        thread=threading.Thread(target=ReceiveFromClient)
+        thread.start()
+        
